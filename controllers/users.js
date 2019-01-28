8637ffd41c42d829d1e3098900d8431e80e5e938
Hannes Schurig <hannes.schurig@hpi.de>
2019-01-28 15:54:35 +0100

    SC-646 Forschungsvorhaben entfernen

--- controllers/users.js
+++ controllers/users.js
@@ -170,7 +170,6 @@ const getCreateHandler = (service) => {
                         pin: pin.pin,
 
                         privacyConsent: true,
-                        researchConsent: true,
                         thirdPartyConsent: true,
                         termsOfUseConsent: true
                     },
