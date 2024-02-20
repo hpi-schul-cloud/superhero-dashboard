b3688e2b4a0abb21e5217ba890fcbfa105602af7
Malte Berg <malte.berg@capgemini.com>
2024-02-20 11:44:26 +0100

    add external Id sort

--- static/scripts/schools.js
+++ static/scripts/schools.js
@@ -107,6 +107,7 @@ $(document).ready(function () {
         'Migration abgeschlossen': 'userLoginMigration.closedAt',
         'Migration final beendet': 'userLoginMigration.finishedAt',
         'Login-System': 'systems',
+        'External Id': 'externalId',
 
         'subject': 'Titel',
         'firstName': 'Vorname',
@@ -135,7 +136,8 @@ $(document).ready(function () {
         'userLoginMigration.mandatorySince': 'Migration verpflichtend',
         'userLoginMigration.closedAt': 'Migration abgeschlossen',
         'userLoginMigration.finishedAt': 'Migration final beendet',
-        'systems': 'Login-System'
+        'systems': 'Login-System',
+        'externalId': 'External Id'
     };
 
     $('tr th').each(function(i,j) {
