624fe89c609104ab421c019623fcca9a7dab2ebf
wolfganggreschus <wolfgang@greschus.de>
2025-01-30 11:14:37 +0100

    Füge Controller und Ansicht für die Batch-Löschung hinzu und aktualisiere die Sidebar

---
+++ controllers/batch-deletion.js
@@ -0,0 +1,27 @@
+/*
+ * One Controller per layout view
+ */
+
+// const _ = require('lodash');
+const express = require('express');
+const router = express.Router();
+const authHelper = require('../helpers/authentication');
+const { api } = require('../api');
+// const moment = require('moment');
+// moment.locale('de');
+
+// secure routes
+router.use(authHelper.authChecker);
+
+router.get('/', function (req, res, next) {
+    res.render('batch-deletion/batch-deletion', {
+        title: 'Löschung',
+        user: res.locals.currentUser,
+        themeTitle: process.env.SC_NAV_TITLE || 'Schul-Cloud'
+    });
+    
+});
+
+
+
+module.exports = router;
--- helpers/authentication.js
+++ helpers/authentication.js
@@ -132,6 +132,11 @@ const restrictSidebar = (req, res) => {
             icon: 'server',
             link: '/storageproviders/'
         },
+        {
+            name: 'Löschung',
+            icon: 'trash',
+            link: '/foo/'
+        },
     ];
 
     res.locals.sidebarItems = res.locals.sidebarItems.filter((item) => item.enabled == null || item.enabled);
