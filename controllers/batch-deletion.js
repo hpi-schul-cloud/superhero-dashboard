57bdda6d458a5c4234bc94fc2e14313120c89a11
Odalys Adam <odalys.adam@dataport.de>
2025-02-03 17:09:43 +0100

    batch-header with dummy data

--- controllers/batch-deletion.js
+++ controllers/batch-deletion.js
@@ -4,10 +4,12 @@
 
 // const _ = require('lodash');
 const express = require("express");
-const multer = require('multer');
+const multer = require("multer");
 const router = express.Router();
 const authHelper = require("../helpers/authentication");
 const { api } = require("../api");
+const moment = require("moment");
+moment.locale("de");
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -15,28 +17,117 @@ const upload = multer({ storage: storage });
 router.use(authHelper.authChecker);
 
 router.get("/", function (req, res, next) {
-  const deletionContainerHead = ["Schüler-IDs", "Lehrer-IDs"];
+  const batches = [
+    {
+      id: "0000d231816abba584714c9c", // batch id
+      status: "created",
+      createdAt: "2024-09-28T11:58:46.601Z",
+      usersByRole: [
+        {
+          roleName: "student",
+          userCount: 42,
+        },
+        {
+          roleName: "teacher",
+          userCount: 3,
+        },
+        {
+          roleName: "admin",
+          userCount: 1,
+        },
+      ],
+    },
+    {
+      id: "0000d231816abba584714c9c", // batch id
+      status: "pending",
+      createdAt: "2024-07-12T11:58:46.601Z",
+      usersByRole: [
+        {
+          roleName: "student",
+          userCount: 42,
+        },
+        {
+          roleName: "teacher",
+          userCount: 3,
+        },
+        {
+          roleName: "admin",
+          userCount: 1,
+        },
+      ],
+    },
+    {
+      id: "0000d231816abba584714c9c", // batch id
+      status: "finished",
+      createdAt: "2024-04-28T11:58:46.601Z",
+      usersByRole: [
+        {
+          roleName: "student",
+          userCount: 42,
+        },
+        {
+          roleName: "teacher",
+          userCount: 3,
+        },
+        {
+          roleName: "admin",
+          userCount: 1,
+        },
+      ],
+    },
+  ];
 
+  const batchesWithFormattedDate = batches.map((batch) => {
+    const studentCount = batch.usersByRole.find(
+      (role) => role.roleName === "student"
+    ).userCount;
+    const teacherCount = batch.usersByRole.find(
+      (role) => role.roleName === "teacher"
+    ).userCount;
+    const adminCount = batch.usersByRole.find(
+      (role) => role.roleName === "admin"
+    ).userCount;
+
+    const overallCount = studentCount + teacherCount + adminCount;
+
+    return {
+      id: batch.id,
+      status: batch.status,
+      createdAt: moment(batch.createdAt).format("DD.MM.YYYY, HH:MM"),
+      studentCount,
+      teacherCount,
+      adminCount,
+      overallCount,
+    };
+  });
+  //   api
+  //     .get("/batch-deletion")
+  //     .then((data) => {
   res.render("batch-deletion/batch-deletion", {
     title: "Sammellöschung von Schülern",
     user: res.locals.currentUser,
     themeTitle: process.env.SC_NAV_TITLE || "Schul-Cloud",
-    deletionContainerHead,
+    batches: batchesWithFormattedDate,
   });
+  //     })
+  //     .catch((err) => {
+  //       next(err);
+  //     });
 });
 
-router.post('/create-batch-deletion-file', upload.single('file'), (req, res, next) => {
-  if (req.file) {
-    console.log('Datei empfangen:', req.file.originalname);
-    const fileBuffer = req.file.buffer;
-    // TODO: Send the file to the API "schulcloud-server"
-    res.redirect(303, '/batch-deletion/');
-  } else {
-    res.status(400).send('Keine Datei hochgeladen');
+router.post(
+  "/create-batch-deletion-file",
+  upload.single("file"),
+  (req, res, next) => {
+    if (req.file) {
+      console.log("Datei empfangen:", req.file.originalname);
+      const fileBuffer = req.file.buffer;
+      // TODO: Send the file to the API "schulcloud-server"
+      res.redirect(303, "/batch-deletion/");
+    } else {
+      res.status(400).send("Keine Datei hochgeladen");
+    }
   }
-});
+);
 
 module.exports = router;
-
-
-
