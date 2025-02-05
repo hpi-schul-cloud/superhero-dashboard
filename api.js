567fc568c1ebb89048c709df08439f7badb60132
Odalys Adam <odalys.adam@dataport.de>
2025-02-05 15:03:09 +0100

    enable & use adminapi

--- api.js
+++ api.js
@@ -1,22 +1,33 @@
-const request = require('request');
-const rp = require('request-promise');
+const request = require("request");
+const rp = require("request-promise");
 
-const api = (req, { useCallback = false, json = true, version = 'v1' } = {}) => {
-    const headers = {};
-    if(req && req.cookies && req.cookies.jwt) {
-        headers['Authorization'] = (req.cookies.jwt.startsWith('Bearer ') ? '' : 'Bearer ') + req.cookies.jwt;
-    }
-    if(process.env.API_KEY) {
-        headers['x-api-key'] = process.env.API_KEY;
-    }
+const api = (
+  req,
+  { useCallback = false, json = true, version = "v1", adminApi = false } = {}
+) => {
+  const headers = {};
+  if (req && req.cookies && req.cookies.jwt) {
+    headers["Authorization"] =
+      (req.cookies.jwt.startsWith("Bearer ") ? "" : "Bearer ") +
+      req.cookies.jwt;
+  }
+  // TODO - temporary solution, adjust this before merging,
+  //   if (process.env.API_KEY) {
+  headers["x-api-key"] =
+    process.env.API_KEY || "thisisasupersecureapikeythatisabsolutelysave";
+  //   }
 
-    const baseUrl = process.env.BACKEND_URL || 'http://localhost:3030/api/';
-    const handler = useCallback ? request : rp;
-    return handler.defaults({
-		baseUrl: new URL(version, baseUrl).href,
-        json,
-        headers
-    });
+  const backendUrl = process.env.BACKEND_URL || "http://localhost:3030/api/";
+  const adminApiUrl =
+    process.env.ADMIN_API_URL || "http://localhost:4030/admin/api/";
+
+  const baseUrl = adminApi ? adminApiUrl : backendUrl;
+  const handler = useCallback ? request : rp;
+  return handler.defaults({
+    baseUrl: new URL(version, baseUrl).href,
+    json,
+    headers,
+  });
 };
 
 module.exports = { api };
--- controllers/batch-deletion.js
+++ controllers/batch-deletion.js
@@ -10,7 +10,6 @@ const { api } = require("../api");
 const moment = require("moment");
 moment.locale("de");
 
-
 router.use(authHelper.authChecker);
 
 const getFormattedDate = (date) => {
@@ -19,128 +18,129 @@ const getFormattedDate = (date) => {
 };
 
 router.get("/", function (req, res, next) {
-  const batches = [
-    {
-      id: "0000d231816abba584714c9c", // batch id
-      status: "created",
-      createdAt: "2024-09-28T11:58:46.601Z",
-      usersByRole: [
+  // console.log(req);
+  api(req, { adminApi: true })
+    .get(`/deletion-batches`)
+    .then((data) => {
+      // const batches = data.batches;
+      console.log("hi", data);
+      const batches = [
         {
-          roleName: "student",
-          userCount: 42,
+          id: "0000d231816abba584714c9c", // batch id
+          status: "created",
+          createdAt: "2024-09-28T11:58:46.601Z",
+          usersByRole: [
+            {
+              roleName: "student",
+              userCount: 42,
+            },
+            {
+              roleName: "teacher",
+              userCount: 3,
+            },
+            {
+              roleName: "admin",
+              userCount: 1,
+            },
+          ],
         },
         {
-          roleName: "teacher",
-          userCount: 3,
+          id: "0000d231816abba584714c9d", // batch id
+          status: "pending",
+          createdAt: "2024-07-12T11:58:46.601Z",
+          usersByRole: [
+            {
+              roleName: "student",
+              userCount: 42,
+            },
+            {
+              roleName: "teacher",
+              userCount: 3,
+            },
+            {
+              roleName: "admin",
+              userCount: 1,
+            },
+          ],
         },
         {
-          roleName: "admin",
-          userCount: 1,
+          id: "0000d231816abba584714c9e", // batch id
+          status: "finished",
+          createdAt: "2024-04-28T11:58:46.601Z",
+          usersByRole: [
+            {
+              roleName: "student",
+              userCount: 42,
+            },
+            {
+              roleName: "teacher",
+              userCount: 3,
+            },
+            {
+              roleName: "admin",
+              userCount: 1,
+            },
+          ],
         },
-      ],
-    },
-    {
-      id: "0000d231816abba584714c9d", // batch id
-      status: "pending",
-      createdAt: "2024-07-12T11:58:46.601Z",
-      usersByRole: [
-        {
-          roleName: "student",
-          userCount: 42,
-        },
-        {
-          roleName: "teacher",
-          userCount: 3,
-        },
-        {
-          roleName: "admin",
-          userCount: 1,
-        },
-      ],
-    },
-    {
-      id: "0000d231816abba584714c9e", // batch id
-      status: "finished",
-      createdAt: "2024-04-28T11:58:46.601Z",
-      usersByRole: [
-        {
-          roleName: "student",
-          userCount: 42,
-        },
-        {
-          roleName: "teacher",
-          userCount: 3,
-        },
-        {
-          roleName: "admin",
-          userCount: 1,
-        },
-      ],
-    },
-  ];
+      ];
 
-  const batchesWithFormattedDate = batches.map((batch) => {
-    const studentCount = batch.usersByRole.find(
-      (role) => role.roleName === "student"
-    ).userCount;
-    const teacherCount = batch.usersByRole.find(
-      (role) => role.roleName === "teacher"
-    ).userCount;
-    const adminCount = batch.usersByRole.find(
-      (role) => role.roleName === "admin"
-    ).userCount;
+      const batchesWithFormattedDate = batches.map((batch) => {
+        const studentCount = batch.usersByRole.find(
+          (role) => role.roleName === "student"
+        ).userCount;
+        const teacherCount = batch.usersByRole.find(
+          (role) => role.roleName === "teacher"
+        ).userCount;
+        const adminCount = batch.usersByRole.find(
+          (role) => role.roleName === "admin"
+        ).userCount;
 
-    const overallCount = studentCount + teacherCount + adminCount;
+        const overallCount = studentCount + teacherCount + adminCount;
 
-    const formattedDate = getFormattedDate(batch.createdAt);
-    const batchTitle = `Sammellöschung vom ${formattedDate} Uhr`;
+        const formattedDate = getFormattedDate(batch.createdAt);
+        const batchTitle = `Sammellöschung vom ${formattedDate} Uhr`;
 
-    const pending = 10;
-    const pendingIds = ["pending-id-1", "pending-id-2"];
-    const deleted = 5;
-    const deletedIds = ["deleted-id-1", "deleted-id-2"];
-    const failed = 2;
-    const failedIds = ["failed-id-1", "failed-id-2"];
+        const pending = 10;
+        const pendingIds = ["pending-id-1", "pending-id-2"];
+        const deleted = 5;
+        const deletedIds = ["deleted-id-1", "deleted-id-2"];
+        const failed = 2;
+        const failedIds = ["failed-id-1", "failed-id-2"];
 
-    return {
-      id: batch.id,
-      status: batch.status,
-      createdAt: formattedDate,
-      batchTitle,
-      studentCount,
-      teacherCount,
-      adminCount,
-      overallCount,
-      pending,
-      pendingIds,
-      deleted,
-      deletedIds,
-      failed,
-      failedIds,
-    };
-  });
-  //   api
-  //     .get("/batch-deletion")
-  //     .then((data) => {
-  res.render("batch-deletion/batch-deletion", {
-    title: "Sammellöschung von Schülern",
-    user: res.locals.currentUser,
-    themeTitle: process.env.SC_NAV_TITLE || "Schul-Cloud",
-    batches: batchesWithFormattedDate,
-  });
-  //     })
-  //     .catch((err) => {
-  //       next(err);
-  //     });
+        return {
+          id: batch.id,
+          status: batch.status,
+          createdAt: formattedDate,
+          batchTitle,
+          studentCount,
+          teacherCount,
+          adminCount,
+          overallCount,
+          pending,
+          pendingIds,
+          deleted,
+          deletedIds,
+          failed,
+          failedIds,
+        };
+      });
+
+      res.render("batch-deletion/batch-deletion", {
+        title: "Sammellöschung von Schülern",
+        user: res.locals.currentUser,
+        themeTitle: process.env.SC_NAV_TITLE || "Schul-Cloud",
+        batches: batchesWithFormattedDate,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
-router.post(
-  "/create-batch-deletion-file",
-  (req, res, next) => {
-      console.log("Post-Router triggered");
-      console.log("body:", req.body);
-      res.redirect(303, "/batch-deletion/");
-  }
-);
+router.post("/create-batch-deletion-file", (req, res, next) => {
+  console.log("Post-Router triggered");
+  console.log("body:", req.body);
+  res.redirect(303, "/batch-deletion/");
+});
 
 module.exports = router;
