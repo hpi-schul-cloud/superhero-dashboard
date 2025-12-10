8aadd7574c1ae33811e95ebc5497ec163333efc9
dyedwiper <maximilian.becker@dataport.de>
2025-12-10 11:45:11 +0100

    Remove outdated code regarding notification service

--- controllers/account.js
+++ controllers/account.js
@@ -33,32 +33,10 @@ router.post('/', async function (req, res) {
 });
 
 router.get('/', function (req, res, next) {
-    if (process.env.NOTIFICATION_SERVICE_ENABLED) {
-        api(req).get('/notification/devices')
-            .then(device => {
-                device.map(d => {
-                    if (d.token === req.cookies.deviceToken) {
-                        Object.assign(d, {selected: true});
-                    }
-                    return d;
-                });
-                res.render('account/settings', {
-                    title: 'Dein Account',
-                    device,
-                    userId: res.locals.currentUser._id
-                });
-            }).catch(err => {
-            res.render('account/settings', {
-                title: 'Dein Account',
-                userId: res.locals.currentUser._id
-            });
-        });
-    } else {
-        res.render('account/settings', {
-            title: 'Dein Account',
-            userId: res.locals.currentUser._id
-        });
-    }
+    res.render('account/settings', {
+        title: 'Dein Account',
+        userId: res.locals.currentUser._id
+    });
 });
 
 // delete file
