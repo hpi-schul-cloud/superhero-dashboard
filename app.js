05ff7247c69bd37b93cde1faa483bfaded92a815
wolfganggreschus <wolfgang@greschus.de>
2026-05-13 17:17:04 +0200

    Remove the commented-out code for handling the Content-Security-Policy header

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ const app = express();
 app.use(compression());
 app.set('trust proxy', true);
 
-app.use((req, res, next) => {
-    res.setHeader('Content-Security-Policy', "default-src 'self'");
-    next();
-});
+//app.use((req, res, next) => {
+//    res.setHeader('Content-Security-Policy', "default-src 'self'");
+//    next();
+//});
 const themeName = process.env.SC_THEME || 'default';
 // view engine setup
 const handlebarsHelper = require('./helpers/handlebars');
