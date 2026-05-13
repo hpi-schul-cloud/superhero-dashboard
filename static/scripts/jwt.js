7409e969ec6a360b23116dc8611367c6fac80d8a
wolfganggreschus <wolfgang@greschus.de>
2026-05-13 12:10:08 +0200

    Remove redundant copyToClipboard function and directly use navigator.clipboard for copying JWT

--- static/scripts/jwt.js
+++ static/scripts/jwt.js
@@ -1,20 +1,3 @@
-
-const copyToClipboard = async (text) => {
-	try {
-		await navigator.clipboard.writeText(text);
-	} catch (error) {
-		// in case browser doesn't support navigator.clipboard
-		const tempInput = document.createElement('input');
-		tempInput.value = text;
-		tempInput.style.position = 'absolute';
-		tempInput.style.left = '-9999px';
-		document.body.appendChild(tempInput);
-		tempInput.select();
-		document.execCommand('copy');
-		document.body.removeChild(tempInput);
-	}
-};
-
 document.addEventListener('DOMContentLoaded', () => {
     const btn = document.querySelector('.btn-copy-jwt');
     const el = document.querySelector('#jwt');
@@ -23,6 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     btn.addEventListener('click', async () => {
         const value = el.value ? el.value.trim() : el.textContent.trim();
-        await copyToClipboard(value);
+        await navigator.clipboard.writeText(value);
     });
 });
