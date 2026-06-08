025ebaa3f13da1b23e24042da2b155ff642eb6e0
dyedwiper <maximilian.becker@dataport.de>
2026-06-08 14:00:07 +0200

    Remove unused middleware

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// only execute middleware on this router
-const handlebarsHelper = require('../helpers/handlebars');
-//router.use(handlebarsHelper.middleware);
-
 router.use(require('./login'));
 router.use(express.json());
 router.use('/dashboard/', require('./dashboard'));
