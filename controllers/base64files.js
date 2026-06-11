ab5925174dda55e301c97d7496a256b0b993821a
Uwe Ilgenstein <uwe.ilgenstein@dataport.de>
2026-06-11 12:28:42 +0200

    remove unnecessary promisification

--- controllers/base64files.js
+++ controllers/base64files.js
@@ -5,9 +5,7 @@ const router = express.Router();
 
 router.get('/:id', async (req, res, next) => {
 	try {
-		const base64File = await Promise.resolve(
-			api(req).get(`/base64Files/${req.params.id}`),
-		);
+		const base64File = await api(req).get(`/base64Files/${req.params.id}`);
 		const fileData = base64File.data;
 		res.json(fileData);
 	} catch (err) {
